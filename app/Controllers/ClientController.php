1fd19caa3b63eec685403a5faeda97a5a3c80924
nomenaidealy <216542558+nomenaidealy@users.noreply.github.com>
2026-07-20 10:05:22 +0300

    commit : clientController

---
+++ app/Controllers/ClientController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\ClientModel;
+use App\Models\PrefixModel ;
+
+class ClientController extends BaseController
+{
+    public function index()
+    {
+        $model = new ClientModel();
+        $data['clients'] = $model->getClient();
+        return view('clients/index', $data);
+    }
+
+    public function loginPage(){
+        return view('auth/login') ;
+    }
+
+    public function login()
+    {
+        $model = new ClientModel();
+        $prefixModel = new PrefixModel() ;
+
+        if ($this->request->getMethod() === 'post') {
+           
+            $telephone = $this->request->getPost('telephone');
+            $telephone = preg_replace('/\D/', '', $telephone);
+
+            if (strlen($telephone) != 10) {
+                return redirect()->back()
+                    ->withInput()
+                    ->with('error', 'Numéro de téléphone non valide');
+            }
+
+            $client = $model->getClient($telephone);
+
+            if ($client) {
+                return redirect()->to('/clients');
+            }
+
+            $prefix = substr($telephone, 0, 3);
+
+            $prefixData = $prefixModel->findByPrefixe($prefix);
+
+            if (!$prefixData) {
+                return redirect()->back()
+                    ->withInput()
+                    ->with('error', 'Préfixe non valide');
+            }
+
+            $prefix_id = $prefixData['id'];
+
+            $model->createClient($prefix_id, $telephone);
+
+            return redirect()->to('/clients');
+        }
+
+        return view('clients/create');
+    }
+}
---
+++ app/Views/auth/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="login" method= "post">
+        <label for="telephone">Entrer vos numero telephone</label>
+        <input type="text" >
+
+        <input type="submit" value="Se connecter">
+    </form>
+</body>
+</html>
