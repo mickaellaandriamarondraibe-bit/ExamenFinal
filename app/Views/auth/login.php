decf4dd884b54f3bb0fef63797d99803f680506a
nomenaidealy <216542558+nomenaidealy@users.noreply.github.com>
2026-07-20 10:05:22 +0300

    commit : clientController

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -1,29 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
+    <form action="login" method= "post">
+        <label for="telephone">Entrer vos numero telephone</label>
+        <input type="text" >
 
-<?php if (session()->getFlashdata('error')) : ?>
-    <div style="color: red;">
-        <?= session()->getFlashdata('error') ?>
-    </div>
-<?php endif; ?>
-
-<form action="<?= site_url('login') ?>" method="post">
-    <label for="telephone">Entrer votre numéro de téléphone</label>
-
-    <input
-        type="text"
-        name="telephone"
-        id="telephone"
-        value="<?= old('telephone') ?>"
-    >
-
-    <input type="submit" value="Se connecter">
-</form>
+        <input type="submit" value="Se connecter">
+    </form>
 </body>
 </html>
