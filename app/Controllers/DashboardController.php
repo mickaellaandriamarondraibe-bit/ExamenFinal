1391261862819ca21a810a1f0d0f41a95a705ac2
MickaElla <mickaellaandriamarondraibe@gmail.com>
2026-07-20 20:17:42 +0300

    correction

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\CompteModel;
+use App\Models\TransactionModel;
 
 class DashboardController extends BaseController
 {
@@ -24,6 +25,20 @@ class DashboardController extends BaseController
         }
 
         $data['compte'] = $compte;
+        $data['derniereTransaction'] = null;
+
+        $transactionModel = new TransactionModel();
+        $transactions = $transactionModel->getHistorique($compte['id']);
+
+        if (!empty($transactions)) {
+            $data['derniereTransaction'] = $transactions[0];
+
+            if ($transactions[0]['compte_destination_id'] == $compte['id']) {
+                $data['derniereTransaction']['sens'] = 'in';
+            } else {
+                $data['derniereTransaction']['sens'] = 'out';
+            }
+        }
 
         return view('dashboard/index', $data);
     }
