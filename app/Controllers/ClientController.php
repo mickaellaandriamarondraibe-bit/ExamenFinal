d15fd9867123d7c52e6a85415df54105ada1dc10
nomenaidealy <216542558+nomenaidealy@users.noreply.github.com>
2026-07-20 10:36:06 +0300

    client

--- app/Controllers/ClientController.php
+++ app/Controllers/ClientController.php
@@ -21,44 +21,38 @@ class ClientController extends BaseController
     }
 
     public function login()
-    {
-        $model = new ClientModel();
-        $prefixModel = new PrefixModel() ;
+{
+    $model = new ClientModel();
+    $prefixModel = new PrefixModel();
 
-        if ($this->request->getMethod() === 'post') {
-           
-            $telephone = $this->request->getPost('telephone');
-            $telephone = preg_replace('/\D/', '', $telephone);
+    if ($this->request->is('post')) {
+        $telephone = $this->request->getPost('telephone');
+        $telephone = preg_replace('/\D/', '', $telephone);
 
-            if (strlen($telephone) != 10) {
-                return redirect()->back()
-                    ->withInput()
-                    ->with('error', 'Numéro de téléphone non valide');
-            }
+        if (strlen($telephone) != 10) {
+            return redirect()->back()
+                ->withInput()
+                ->with('error', 'Numéro de téléphone non valide');
+        }
 
-            $client = $model->getClient($telephone);
-
-            if ($client) {
-                return redirect()->to('/clients');
-            }
-
-            $prefix = substr($telephone, 0, 3);
-
-            $prefixData = $prefixModel->findByPrefixe($prefix);
-
-            if (!$prefixData) {
-                return redirect()->back()
-                    ->withInput()
-                    ->with('error', 'Préfixe non valide');
-            }
-
-            $prefix_id = $prefixData['id'];
-
-            $model->createClient($prefix_id, $telephone);
-
+        $client = $model->getClient($telephone);
+        if ($client) {
             return redirect()->to('/clients');
         }
 
-        return view('clients/create');
+        $prefix = substr($telephone, 0, 3);
+        $prefixData = $prefixModel->findByPrefixe($prefix);
+        if (!$prefixData) {
+            return redirect()->back()
+                ->withInput()
+                ->with('error', 'Préfixe non valide');
+        }
+
+        $prefix_id = $prefixData['id'];
+        $model->createClient($prefix_id, $telephone);
+        return redirect()->to('/clients');
     }
+
+    return view('auth/welcom');
+}
 }
